2b9c5dcfdcba8d7a0dea0e93da1e0be6ffe86ee2
James Toyer <james@glipho.com>
2013-03-26 15:21:01 +0000

    Added some default modules

--- modules/glipho/manifests/environment.pp
+++ modules/glipho/manifests/environment.pp
@@ -1,3 +1,15 @@
 class glipho::environment {
 	include chrome
+	include firefox
+	include sublime_text_2
+	include java
+	include vagrant
+	vagrant::plugin { '
+	vagrant::plugin { 'vagrant-vmware-fusion':
+		license => 'puppet:///modules/people/joe/licenses/fusion.lic',
+	}
+
+	vagrant::box { 'squeeze64/vmware_fusion':
+		source => 'https://s3.amazonaws.com/github-ops/vagrant/squeeze64-6.0.7-vmware_fusion.box'
+	}
 }
