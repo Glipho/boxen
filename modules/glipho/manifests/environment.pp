5ab74e3e2e1bf81e26f0c4dffb84ca00152e6342
James Toyer <james@glipho.com>
2013-03-26 15:47:05 +0000

    Sorted out vagrant install

--- modules/glipho/manifests/environment.pp
+++ modules/glipho/manifests/environment.pp
@@ -3,12 +3,6 @@ class glipho::environment {
 	include firefox
 	include sublime_text_2
 	include java
+	include virtualbox
 	include vagrant
-	vagrant::plugin { 'vagrant-vmware-fusion':
-		license => 'puppet:///modules/people/joe/licenses/fusion.lic',
-	}
-
-	vagrant::box { 'squeeze64/vmware_fusion':
-		source => 'https://s3.amazonaws.com/github-ops/vagrant/squeeze64-6.0.7-vmware_fusion.box'
-	}
 }
